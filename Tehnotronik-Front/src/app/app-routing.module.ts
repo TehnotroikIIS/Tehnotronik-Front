2caa044437d2cc71c52aedcc6fa7b8f8c062b51c
Natalija Krsmanović <64694174+KNatali@users.noreply.github.com>
2022-06-15 23:52:56 +0200

    add contact page

--- Tehnotronik-Front/src/app/app-routing.module.ts
+++ Tehnotronik-Front/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { AboutUsComponent } from './home/about-us/about-us.component';
+import { ContactComponent } from './home/contact/contact.component';
 import { HomePageComponent } from './home/home-page/home-page.component';
 
 const routes: Routes = [
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'about-us',
     component: AboutUsComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   },
 ];
 
--- Tehnotronik-Front/src/app/home/home.module.ts
+++ Tehnotronik-Front/src/app/home/home.module.ts
@@ -4,13 +4,15 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { SharedModule } from '../shared/shared.module';
 import { NavbarUnregisteredComponent } from '../shared/navbar-unregistered/navbar-unregistered.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    ContactComponent
   ],
   imports: [
     CommonModule,
