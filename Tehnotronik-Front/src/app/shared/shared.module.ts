181828c517e288b3877c4c012983973e07b092cb
Natalija Krsmanović <64694174+KNatali@users.noreply.github.com>
2022-06-15 18:50:58 +0200

    add navbar with fileds

--- Tehnotronik-Front/src/app/shared/shared.module.ts
+++ Tehnotronik-Front/src/app/shared/shared.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarUnregisteredComponent } from './navbar-unregistered/navbar-unregistered.component';
+import { AppRoutingModule } from '../app-routing.module';
+
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 
@@ -9,7 +15,15 @@ import { NavbarUnregisteredComponent } from './navbar-unregistered/navbar-unregi
     NavbarUnregisteredComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule,
+    MatMenuModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+  ],
+  exports: [
+    NavbarUnregisteredComponent
   ]
 })
 export class SharedModule { }
