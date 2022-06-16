b02e8b40632aab9f1b1877528eb41bad117ef045
Natalija Krsmanović <64694174+KNatali@users.noreply.github.com>
2022-06-16 02:27:07 +0200

    added filters to product page

--- Tehnotronik-Front/src/app/products/all-products/all-products.component.ts
+++ Tehnotronik-Front/src/app/products/all-products/all-products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ProductService } from 'src/app/core/services/product.service';
 
 @Component({
   selector: 'app-all-products',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-products.component.scss']
 })
 export class AllProductsComponent implements OnInit {
-
-  constructor() { }
+  allProducts:any[]=[]
+   products:any[]=[{
+    name:'Ferit za kablove',
+    price:256,
+    description:'desciriton',
+    manufacturer:'manufac',
+    techicalDescription:'technical',
+    categoryId:'id',
+    rate:8,
+    numberOfReviews:10,
+    isAvailable:1
+   }]
+  constructor(
+    private productService:ProductService
+  ) { }
 
   ngOnInit(): void {
+    this.getAllProducts();
+  }
+  sort = new FormControl();
+  industry = new FormControl();
+  age = new FormControl();
+  sortList: string[] = ['Od najniže cene','Od najviše cene'];
+  industryList: string[] = ['Svi','Dostupni'];
+  ageList: string[] = ['<500', '500-3000', '3000-10000', '>10000']
+  noExperienceList: any[] = []
+
+  getAllProducts(){
+    this.productService.getAllproducts().subscribe(data=>{
+      this.allProducts=data;
+      console.log(this.allProducts)
+    },error=>{
+      alert('Greska!')
+    })
   }
 
 }
--- Tehnotronik-Front/src/app/products/products.module.ts
+++ Tehnotronik-Front/src/app/products/products.module.ts
@@ -2,6 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { AllProductsComponent } from './all-products/all-products.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { RouterModule } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSelectModule } from '@angular/material/select';
 
 
 
@@ -12,6 +22,16 @@ import { AllProductsComponent } from './all-products/all-products.component';
   imports: [
     CommonModule,
     SharedModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatRadioModule,
+    RouterModule,
+    MatSelectModule,
+    MatTabsModule,
+    MatGridListModule
   ]
 })
 export class ProductsModule { }
