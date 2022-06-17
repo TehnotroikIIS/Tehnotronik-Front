435ba93c12e514513705c13460b3d8f577c4ccb8
anastasijapopovic <popovic.in46.2018@uns.ac.rs>
2022-06-17 20:06:40 +0200

    Add dialog for add to cart

--- Tehnotronik-Front/src/app/auth/user-profile/user-profile.component.ts
+++ Tehnotronik-Front/src/app/auth/user-profile/user-profile.component.ts
@@ -33,6 +33,13 @@ export class UserProfileComponent implements OnInit {
     this.user = this.jwtService.getUserDetails();
     console.log(this.user)
     this.profileForm.get('name')?.setValue(this.user.name);
+    this.profileForm.get('lastname')?.setValue(this.user.lastname);
+    this.profileForm.get('username')?.setValue(this.user.username);
+    this.profileForm.get('email')?.setValue(this.user.email);
+    this.profileForm.get('phoneNumber')?.setValue(this.user.phoneNumber);
+    this.profileForm.get('address')?.setValue(this.user.address);
+    this.profileForm.get('city')?.setValue(this.user.city);
+    this.profileForm.get('state')?.setValue(this.user.state);
   }
 
   changePassword() {}
