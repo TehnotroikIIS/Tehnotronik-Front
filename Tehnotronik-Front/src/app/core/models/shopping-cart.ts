2ec126d6c626ef93d0d826896607fe373a8ea03c
anastasijapopovic <popovic.in46.2018@uns.ac.rs>
2022-06-19 01:41:59 +0200

    Try to fix edit product from cart

--- Tehnotronik-Front/src/app/core/models/shopping-cart.ts
+++ Tehnotronik-Front/src/app/core/models/shopping-cart.ts
@@ -2,5 +2,5 @@ import { ShoppingCartItem } from "./shopping-cart-item";
 
 export interface ShoppingCart {
     userId:string;
-    shoppingCartItem: ShoppingCartItem[];
+    shoppingCartItems: ShoppingCartItem[];
 }
